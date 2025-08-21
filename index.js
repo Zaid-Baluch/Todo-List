ed604c8c0ad1fbe3be9c6f7685cb2bafda6b2cbd
Sohaib Raheem <zaidbeluch@gmail.com>
2025-08-21 15:13:04 +0500

    pending

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var todos = [];
+var editIndex = -1;
 
 function myTodo() {
   var inputElement = document.getElementById("input-box");
-  todos.push("inputElement.value");
-  inputElement.value = " ";
+  todos.push(inputElement.value);
+  inputElement.value = "";
   showlist();
 }
 
@@ -11,6 +12,37 @@ function showlist() {
   var listItem = document.getElementById("list-container");
   listItem.innerHTML = " ";
   for (var i = 0; i < todos.length; i++) {
-    listItem.innerHTML += `<li> ${todos[i]} </li>`;
+    listItem.innerHTML += `
+    <li>
+    ${todos[i]}
+    <button onclick="deleteTodo(${i})"><i class="fa-solid fa-trash"></i></button>
+    <button onclick="editTodo(${i})">Edit</button>
+    </li>`;
   }
+}
+function deleteTodo(index) {
+  todos.splice(index, 1);
+  showlist();
+}
+
+function editTodo() {
+  var inputElement = document.getElementById("input-box");
+  inputElement.value = todos[index];
+  editIndex = index;
+  addBtn.style.display = "none";
+  updateBtn.style.display = "inline-block";
+}
+function updateTodo() {
+  var inputElement = document.getElementById("input-box");
+  if (inputElement.value === "") {
+    alert("please Fill the box");
+  }
+
+  todos[editIndex] = inputElement.value;
+  inputElement.value = "";
+  editIndex = -1;
+
+  addBtn.style.display = "inline-block";
+  updateBtn.style.display = "none";
+  showlist();
 }
